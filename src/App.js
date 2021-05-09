accf3946c567f8f7ba4c17d0c94f841ab4f2f10f
Amit Kumar <amitverma1150@gmail.com>
2021-05-09 18:04:13 +0530

    food app

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import {  useState } from 'react';
+import "./App.css";
+import Cart from './components/Cart/Cart';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+import CartProvider from './store/cart.provider';
+
+const App = () => {
+  console.log('app loaded');
+  const [cartIsShown, setCartIsShown] = useState(false);
+  
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  }
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  }
+
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
